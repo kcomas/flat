aee0d3cf36cc82b71f7d0e7e41c3838a2a35872b
Kevin Comas <kcomas99@gmail.com>
2015-10-21 03:27:33 -0400

    more

--- compile.sh
+++ compile.sh
@@ -3,11 +3,12 @@
 rm -R lib
 rm -R docs
 rm -R docs-public
-rm -R lib-public
+rm ./flat-public/admin/js/*-compiled.js
 
 esdoc -c esdoc.json
-#esdoc -c esdoc-public.json
+esdoc -c esdoc-public.json
 babel ./app -d lib
+babel ./flat-public/admin/js/backend.js -o ./flat-public/admin/js/backend-compiled.js
 #babel ./flat-public  -d lib-public
 mkdir lib/flat-config
 mkdir lib/flat-content
