7c1d11a37c98f0acb7cb30ed877044f9e2f9a0d3
Kevin Comas <kcomas99@gmail.com>
2015-10-21 00:17:22 -0400

    missing

--- app/flat-lib/server/serverLoop.js
+++ app/flat-lib/server/serverLoop.js
@@ -41,9 +41,7 @@ export default class serverLoop {
             //loop through the routers
             var currentRouter = null;
             this.routers.forEach(function(router){
-                console.log(router.path);
-                console.log(self.req.url);
-                if(router.path.indexOf(self.req.url) === 0){
+                if(router.path.indexOf(self.req.url) > -1){
                     currentRouter = router.router;
                 }
             });
