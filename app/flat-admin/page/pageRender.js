fe786d833712756325ccb3be8620a87aea63c270
Kevin Comas <kcomas99@gmail.com>
2015-10-21 01:52:11 -0400

    wrong

--- app/flat-admin/page/pageRender.js
+++ app/flat-admin/page/pageRender.js
@@ -25,8 +25,12 @@ export default function pageRender(dir,permalink,tags,files){
     var container = loadedFiles.shift().str;
     console.dir(container);
     loadedFiles.forEach(function(file2){
+        try{
             var reg = new RegExp('\['+file2.name+'\]',g);
             container.replace(reg,file2.str);
+        } catch(err){
+            console.log(err);
+        }
     });
     console.dir(container);
     tags.forEach(function(tag){
