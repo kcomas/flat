3b5a3252b3c3022b5fd8efde39b84017a52dbca3
Kevin Comas <kcomas99@gmail.com>
2015-10-21 01:41:22 -0400

    wrong

--- app/flat-admin/page/pageRender.js
+++ app/flat-admin/page/pageRender.js
@@ -16,7 +16,7 @@ export default function pageRender(dir,permalink,tags,files){
     files.forEach(function(file){
         for(var key in file){
             try {
-                loadedFiles.push({name:key,string:fs.readFileSync(file[key],'utf8')});
+                loadedFiles.push({name:key,str:fs.readFileSync(file[key],'utf8')});
             } catch(err){
                 console.log(err);
             }
@@ -24,7 +24,7 @@ export default function pageRender(dir,permalink,tags,files){
     });
     for(var i=1; i<loadedFiles.length; i++){
         try {
-            loadedFiles[0].string.replace('['+loadedFiles[i].name+']',loadedFiles[i].string);
+            loadedFiles[0].str.replace('['+loadedFiles[i].name+']',loadedFiles[i].string);
         } catch(err){
             console.log(err);
         }
@@ -33,7 +33,7 @@ export default function pageRender(dir,permalink,tags,files){
     tags.forEach(function(tag){
         for(key in tag){
             try {
-                loadedFiles[0].string.replace('['+key+']',tag[key]);
+                loadedFiles[0].str.replace('['+key+']',tag[key]);
             } catch(err){
                 console.log(err);
             }
