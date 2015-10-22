a09637c5f210b6f8429c1c75a610dbbfeba18f6f
Kevin Comas <kcomas99@gmail.com>
2015-10-22 01:02:52 -0400

    new

--- app/flat-lib/page/page.js
+++ app/flat-lib/page/page.js
@@ -13,19 +13,19 @@ export default class page extends item {
      * Create a new page
      * @overide
      * @param {string} permalink - the link of the page
-     * @param {object} default - the default data to be passed into the page object as key:value
+     * @param {object} def - the default data to be passed into the page object as key:value
      * @param {string} template - the name of the template file
      * @param {function(err:err,done:boolean)} callback - returns done true if created
      * @return {function} the callback function
      */
-    create(permalink,default,template,callback){
+    create(permalink,def,template,callback){
         var self = this;
         this.genId(function(err,done){
             if(err){
                 return callback(err,null);
             }
             self.data.permalink = permalink;
-            self.data.default = default;
+            self.data.def = def;
             self.data.template = template;
             self.data.dateCreated = new Date();
             self.save(function(err,done){
