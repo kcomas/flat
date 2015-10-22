5cb153d608e86f97a6e9f9ac682ba9e728ce3c7f
Kevin Comas <kcomas99@gmail.com>
2015-10-22 15:58:07 -0400

    page

--- flat-public/globals/admin/js/sectionEditor.js
+++ flat-public/globals/admin/js/sectionEditor.js
@@ -1,4 +1,16 @@
 
+//init tinymce
+tinymce.init({
+    selector:'#tinymce',
+    plugins: [
+         "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+     "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+     "save table contextmenu directionality emoticons template paste textcolor"
+   ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons"
+});
+
+
 app.controller('adminSectionEdit',['$scope','$http',function($scope,$http){
     
     $scope.sectionList = [];
