058764ef4d29068611bdbfbdebc3a837069581bc
Kevin Comas <kcomas99@gmail.com>
2015-10-23 00:07:39 -0400

    section

--- app/flat-lib/helpers/items/manager.js
+++ app/flat-lib/helpers/items/manager.js
@@ -44,6 +44,7 @@ export default class manager {
         var files = fs.readDirSync(this.dir);
         var self = this;
         files.forEach(function(file){
+            console.log(file);
             var data = fs.readFileSync(self.dir+file,'utf8');
             try {
                 data = JSON.parse(data);
