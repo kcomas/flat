4615282c6001b3fbcad43170d627880eb0a4f39b
Kevin Comas <kcomas99@gmail.com>
2015-10-22 20:38:05 -0400

    saving

--- flat-public/globals/admin/js/sectionEditor.js
+++ flat-public/globals/admin/js/sectionEditor.js
@@ -28,14 +28,14 @@ app.controller('adminSectionEdit',['$scope','$http',function($scope,$http){
     };
 
     $scope.save = function(){
-        var data = $.param({
+        var jsonData = $.param({
             json: JSON.stringify({
                 name : $scope.current.section.name,
                 content : $scope.current.section.content
             })
         });
 
-        $http.post('/flat-admin/upsert-section',data).success(data,status){
+        $http.post('/flat-admin/upsert-section',jsonData).success(data,status){
 
         });
 
