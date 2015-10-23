836091a21f44195e2358ef256f57213b9883f995
Kevin Comas <kcomas99@gmail.com>
2015-10-23 12:11:22 -0400

    template

--- flat-public/globals/admin/js/sectionEditor.js
+++ flat-public/globals/admin/js/sectionEditor.js
@@ -2,12 +2,9 @@
 app.controller('adminSectionEdit',['$scope','$http',function($scope,$http){
     
     $scope.tinymceOptions = {
-        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons",
-        plugins: [
-             "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
-         "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
-         "save table contextmenu directionality emoticons template paste textcolor"
-       ]
+        minHeight: 400,
+        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons", 
+        plugins : "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking save table contextmenu directionality emoticons template paste textcolor autoresize"
     };
 
     //the current section we are edtiting
