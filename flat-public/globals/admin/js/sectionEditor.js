88a9f125ef0a2e9da0ef9c8fae2e11fbd06c1ada
Kevin Comas <kcomas99@gmail.com>
2015-10-23 00:46:53 -0400

    section

--- flat-public/globals/admin/js/sectionEditor.js
+++ flat-public/globals/admin/js/sectionEditor.js
@@ -4,9 +4,9 @@
 app.controller('adminSectionEdit',['$scope','$http',function($scope,$http){
     
     $scope.tinymceOptions = {
-        height:500,
+        minHeight:500,
         toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | print preview media fullpage | forecolor backcolor emoticons", 
-        plugins : "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking save table contextmenu directionality emoticons template paste textcolor",
+        plugins : "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking save table contextmenu directionality emoticons template paste textcolor"
     };
 
     $scope.save = {};
