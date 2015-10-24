40e0ba72c92b7848d325b05fb619dbce8de89a99
Kevin Comas <kcomas99@gmail.com>
2015-10-24 00:48:43 -0400

    page

--- flat-public/globals/admin/js/pageEditor.js
+++ flat-public/globals/admin/js/pageEditor.js
@@ -81,9 +81,9 @@ app.controller('adminPageEdit',['$scope','$http',function($scope,$http){
             //merge the page defs with the part
             if($scope.page.def[i].name === part[0]){
                 if($scope.page.def[i].type === 'text'){
-                    var obj = {'name':part[0],'type':partA[0],'text':$scope.page.def[part[0]].text,'html':$scope.page.def[part[0]].text,'index':i}; 
+                    var obj = {'name':part[0],'type':partA[0],'text':$scope.page.def[i].text,'html':$scope.page.def[i].text,'index':i}; 
                 } else if($scope.page.def[part[0]].type === 'html'){
-                    var obj = {'name':part[0],'type':partA[0],'text':$scope.page.def[part[0]].html,'html':$scope.page.def[part[0]].html,'index':i}; 
+                    var obj = {'name':part[0],'type':partA[0],'text':$scope.page.def[i].html,'html':$scope.page.def[i].html,'index':i}; 
                 }
             } else {
                 var obj = {'name':part[0],'type':partA[0],'text':partA[1],'html':partA[1],'index':i}; 
