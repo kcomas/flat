709f7a80e9e43b442e90596d25cf0d38d94ec27b
Kevin Comas <kcomas99@gmail.com>
2015-10-25 04:00:16 -0400

    page

--- app/flat-lib/helpers/writeFile.js
+++ app/flat-lib/helpers/writeFile.js
@@ -12,8 +12,7 @@ import fs from 'fs';
  * @returns {function} the callback function
  */
 export default function writeFile(dir,name,fileData,callback){
-    var buf = new Buffer(fileData);
-    fs.writeFile(dir+name,buf,function(err){
+    fs.writeFile(dir+name,fileData,function(err){
         if(err){
             return callback(err,null);
         }
