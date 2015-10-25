80c3f7f3b6912e7856feb69a1b326e399e8cc2f4
Kevin Comas <kcomas99@gmail.com>
2015-10-24 23:43:12 -0400

    page

--- app/flat-admin/adminRouter.js
+++ app/flat-admin/adminRouter.js
@@ -230,7 +230,8 @@ adminRouter.post('/flat-admin/upsert-page',function(req,res){
 
 //upload a file
 adminRouter.post('/flat-admin/upload',function(req,res){
-    console.log(req.body);
+    var buf = new Buffer(req.body);
+    console.dir(buf.toString());
 });
 
 adminRouter.always(function(req,res){
