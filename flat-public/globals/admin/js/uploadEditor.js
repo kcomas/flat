00a0fa8d1894c71dc2376394011c957ebd1c040a
Kevin Comas <kcomas99@gmail.com>
2015-10-24 22:54:57 -0400

    page

--- flat-public/globals/admin/js/uploadEditor.js
+++ flat-public/globals/admin/js/uploadEditor.js
@@ -6,6 +6,7 @@ app.controller('adminUpload',['$scope','$http',function($scope,$http){
     $scope.action.status = null;
     $scope.action.msg = '';
     $scope.current.public = false;
+    var fileSelect = document.getElementById('fileToBeUploaded');
 
     //clear the form
     $scope.clear = function(){
@@ -13,6 +14,7 @@ app.controller('adminUpload',['$scope','$http',function($scope,$http){
         $scope.current.public = false;
         $scope.action.status = null;
         $scope.action.msg = '';
+        fileSelect.files = null;
     };
 
 }]);
