b37ac211b16e2af97121c6b88a4a644041153af4
Kevin Comas <kcomas99@gmail.com>
2015-10-25 10:21:17 -0400

    new

--- app/flat-lib/helpers/determineEncoding.js
+++ app/flat-lib/helpers/determineEncoding.js
@@ -7,6 +7,13 @@
  * @return {string} the encoding to use
  */
 export default function determineEncoding(mime){
-
-
+    var encoding = 'binary';
+    var textTypes = ['text','json','javascript'];
+    for(let i=0,l=textTypes.length; i<l; i++){
+        if(mime.indexOf(textTypes[i]) > -1){
+            encoding = 'utf-8';
+            break;
+        }
+    }
+    return encoding;
 }
--- app/flat-lib/server/loadBody.js
+++ app/flat-lib/server/loadBody.js
@@ -2,11 +2,11 @@
 "use strict"
 
 import qs from 'querystring';
-import fs from 'fs';
 
 /**
  * Load a file upload or other multipart data
  * @param {object} req - the request data
+ * @param {number} maxPostSize - the maximum upload size
  * @param {function(req:object)} callback - returns the request obejct in a callback
  * @return {function} the callback function
  */
@@ -26,11 +26,69 @@ function loadFile(req,maxPostSize,callback){
     
     req.on('end',function(){
         body = body.toString('binary');
-        console.dir(body);
-        fs.writeFileSync('../flat-public/uploads/file',body,'binary');
-    });
+        req.body = {};
+        req.body.files = {};
+        var string = string.split('Content-Disposition:');
+
+		string.shift();
+
+
+		var reg = new RegExp('(\r\n|\r|\n)', 'g');
+
+		string.forEach(function (str) {
+			str = str.replace('form-data;', '')
+		    str = str.substring(0, str.indexOf('\r\n--'));
+		    str = str.split('\r\n\r\n');
+		    var obj = {};
+		    if (str[0].indexOf('"; filename="') > -1) {
+		        obj.type = 'file';
+		        var sub = str[0].split('"; filename="');
+		        var name = sub[0].split('="')[1].replace(reg, '')
+		        req.body.files[name] = {}; //name
+		        req.body.files[name].filename = type[0].replace(reg, '');
+		        req.body.files[name].value = str[1].replace(reg, '');
+		    } else {
+		        var sub = str[0].split('=');
+		        sub = sub[1].replace(/"/g, '');
+		        req.body[sub.replace(reg, '')] = str[1].replace(reg, '');
+			}
+	    });
+	    return callback(req);
 }
 
+/**
+ * Load a post string
+ * @param {object} req - the request object
+ * @param {number} maxPostSize - the maximum upload size
+ * @param {function(req:object)} callback - returns the request obejct in a callback
+ * @return {function} the callback function
+ */
+function loadString(req,maxPostSize,callback){
+    var body = '';
+
+    req.on('data',function(data){
+        console.dir(data);
+        body += data;
+        if(body.length > maxPostSize){
+            //destory the connection
+            req.connection.destroy();
+        }
+    });
+
+    req.on('error',function(){
+        return callback(req);
+    });
+    
+    req.on('end',function(){
+        try {
+            req.body = JSON.parse(body);
+            return callback(req);
+        } catch(err){
+            req.body = qs.parse(body);
+            return callback(req);
+        }
+    });
+}
 
 /**
  * This function loads the post body data if the method is post
@@ -53,33 +111,9 @@ export default function loadBody(req,res,maxPostSize,callback){
         });
     } else {
         //load string
+        loadString(req,maxPostSize,function(req){
+            return callback();
+        });
     }
 
-    /*
-    var body = '';
-
-    req.on('data',function(data){
-        console.dir(data);
-        body += data;
-        if(body.length > maxPostSize){
-            //destory the connection
-            req.connection.destroy();
-        }
-    });
-
-    req.on('error',function(){
-        return callback();
-    });
-    
-    req.on('end',function(){
-            try {
-                req.body = JSON.parse(body);
-                return callback();
-            } catch(err){
-                req.body = qs.parse(body);
-                return callback();
-            }
-        }
-    });
-    */
 }
