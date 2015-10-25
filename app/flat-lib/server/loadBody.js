1521a6dbfc0bbcbab2437f95b4411d765a7ec2e9
Kevin Comas <kcomas99@gmail.com>
2015-10-25 07:40:22 -0400

    new

--- app/flat-lib/server/loadBody.js
+++ app/flat-lib/server/loadBody.js
@@ -40,7 +40,7 @@ var reg = new RegExp('(\r\n|)', 'g');
 	                arr2[idx] = str[1].charCodeAt(idx) & 0xFF;
 	            }
 	            */
-	            obj.value = str[1];
+	            obj.value = str[1].toString('binary');
 	        }
 	    } else {
 	        obj.type = 'string'
