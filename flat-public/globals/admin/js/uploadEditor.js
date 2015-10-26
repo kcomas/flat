e66dd565d7ca4cfefe474ffcaa9f830f45ccd1c4
Kevin Comas <kcomas99@gmail.com>
2015-10-26 07:49:41 -0400

    fixed

--- flat-public/globals/admin/js/uploadEditor.js
+++ flat-public/globals/admin/js/uploadEditor.js
@@ -55,7 +55,7 @@ app.controller('adminUpload',['$scope','$http',function($scope,$http){
 
         } else {
             dir = dir.replace(public_remove,'');
-            var win = window.open(dir+fileName);
+            var win = window.open(dir+obj.fileName);
             win.focus();
         }
     };
