c7356e585f7d0c507bc40fc0b2d7cf2d7318df0e
Kevin Comas <kcomas99@gmail.com>
2015-10-27 13:03:09 -0400

    install

--- app/flat-lib/server/responseMods.js
+++ app/flat-lib/server/responseMods.js
@@ -8,6 +8,7 @@ import fs from 'fs';
  * This function adds new methods to the response object
  * @param {object} req - the request object
  * @param {object} res - the response object
+ * @param {string} staticDir - the static file dir
  */
 export default function responseMods(req,res,staticDir){
         //redirect to a url on the site
