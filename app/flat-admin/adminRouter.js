65702ac25f5f0ef7dd6aba5934b759262c268d5e
Kevin Comas <kcomas99@gmail.com>
2015-10-28 14:59:41 -0400

    new

--- app/flat-admin/adminRouter.js
+++ app/flat-admin/adminRouter.js
@@ -241,11 +241,7 @@ adminRouter.post('/flat-admin/remove-page',(req,res)=>{
         } else {
             //delete the cached page if it exists
             adminRouter.controller.cacheManager.removeByParam('permalink',req.body.permalink,(err,done)=>{
-                if(err){
-                    showError(req,res,err,500);
-                } else {
-                    showSuccess(req,res,"item deleted",200);
-                }
+                showSuccess(req,res,"item deleted",200);
             });
         }
     });
