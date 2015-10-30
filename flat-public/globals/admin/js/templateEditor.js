5e37b026448ba06e2ac4ea72e6fa7dd36e2e3f54
Kevin Comas <kcomas99@gmail.com>
2015-10-30 11:03:12 -0400

    trying to fix visual editor

--- flat-public/globals/admin/js/templateEditor.js
+++ flat-public/globals/admin/js/templateEditor.js
@@ -68,7 +68,7 @@ app.controller('adminTemplateEdit',['$scope','$http',function($scope,$http){
     $scope.removeElement = function(visualItem,index){
         console.dir(visualItem);
         console.dir(index);
-        visualItem.children.splice(index,1);        
+        visualItem.splice(index,1);        
     };
 
     $scope.load = function(){
