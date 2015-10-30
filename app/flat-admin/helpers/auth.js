9080c5045d4cbb929d73d3434e1145a1cc315569
Kevin Comas <kcomas99@gmail.com>
2015-10-30 13:47:40 -0400

    changed auth

--- app/flat-admin/helpers/auth.js
+++ app/flat-admin/helpers/auth.js
@@ -10,6 +10,7 @@
 function needsAuth(authList,url){
     for(let i=0,l=authList.length; i<l; i++){
         if(url.indexOf(authList[i]) === 0){
+            console.log("needs auth");
             return true;
         }
     }
