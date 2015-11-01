9d05e67aabb71d7e4a2be3e78a58a2c9dbfeea3f
Kevin Comas <kcomas99@gmail.com>
2015-11-01 15:29:54 -0500

    index admin page

---
+++ flat-public/globals/admin/js/indexEditor.js
@@ -0,0 +1,43 @@
+
+app.controller('indexEdit',['$scope','$http',function($scope,$http){
+    
+
+    //the current section we are edtiting
+    $scope.current = {};
+    $scope.action = {};
+    $scope.action.status = null;
+    $scope.action.msg = '';
+
+
+    $scope.load = function(){
+    };
+
+    //load all of the sections
+    $scope.load();
+
+    function getItem(name,arr){
+        var index = -1;
+        for(var i=0,l=arr.length; i<l; i++){
+            if(arr[i].name === name){
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    $scope.clear = function(){
+        $scope.action.status = null;
+        $scope.action.msg = '';
+    };
+
+
+    $scope.save = function(name){
+
+    };
+
+   
+
+}]);
+
+
