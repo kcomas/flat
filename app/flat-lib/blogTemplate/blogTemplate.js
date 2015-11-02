028e01ef40f8febfd431e2c955b6132d703916b3
Kevin Comas <kcomas99@gmail.com>
2015-11-02 14:18:45 -0500

    stat issue

--- app/flat-lib/blogTemplate/blogTemplate.js
+++ app/flat-lib/blogTemplate/blogTemplate.js
@@ -25,6 +25,7 @@ export default class blogTemplate {
         try {
             var stat = statSync(this._dir);
         } catch(err){
+            console.dir(err);
             fs.mkdirSync(this._dir);
         }
 
